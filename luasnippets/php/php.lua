20eac0722fe7051f61863fd37ecffe976bb5b699
Ben Everly <ben.everly@oberd.com>
2023-07-25 22:33:18 -0500

    initial snippets

--- luasnippets/php/php.lua
+++ luasnippets/php/php.lua
@@ -1,3 +1,84 @@
 return {
-	s('test', { t('this is a snippet') }),
+	s('class', fmt(
+		[[
+			<?php
+
+			namespace {};
+
+			class {}
+			{{
+				{}
+			}}
+		]],
+		{
+			f(require 'luasnip-snippets.php-helpers'.getNamespaceForFile),
+			f(require 'luasnip-snippets.php-helpers'.getClassnameForFile),
+			i(0),
+		}
+	)),
+	s('public', fmt(
+		[[
+			public function {}({}) : {}
+			{{
+				{}
+			}}
+		]],
+		{
+			i(1, 'name'),
+			i(2, 'int $param'),
+			i(3, 'void'),
+			i(0, 'return'),
+		}
+	)),
+	s('protected', fmt(
+		[[
+			protected function {}({}) : {}
+			{{
+				{}
+			}}
+		]],
+		{
+			i(1, 'name'),
+			i(2, 'int $param'),
+			i(3, 'void'),
+			i(0, 'return'),
+		}
+	)),
+	s('private', fmt(
+		[[
+			private function {}({}) : {}
+			{{
+				{}
+			}}
+		]],
+		{
+			i(1, 'name'),
+			i(2, 'int $param'),
+			i(3, 'void'),
+			i(0, 'return'),
+		}
+	)),
+	s('function', fmt(
+		[[
+			function ({}) : {}
+			{{
+				{}
+			}}
+		]],
+		{
+			i(1, 'int $param'),
+			i(2, 'void'),
+			i(0, 'return'),
+		}
+	)),
+	s('fn', fmt(
+		[[
+			fn ({}) : {} => {};
+		]],
+		{
+			i(1, 'int $param'),
+			i(2, 'void'),
+			i(0, ''),
+		}
+	))
 }
